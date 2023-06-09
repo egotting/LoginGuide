d5d3be7e8ad8dc94475d3dfb92674c99e4239085
henrico <104780505+fantaasyyy@users.noreply.github.com>
2023-06-09 16:36:53 -0300

    LoginGuide in java

---
+++ src/LoginApp.java
@@ -0,0 +1,9 @@
+package src;
+
+import src.gui.LoginGUI;
+
+public class LoginApp {
+    public static void main(String args[]){
+        new LoginGUI().setVisible(true);
+    }
+}
---
+++ src/constants/CommandConstants.java
@@ -0,0 +1,9 @@
+package src.constants;
+
+public class CommandConstants {
+    public final static String APP_NAME ="Whatsapp2😎";
+
+    public final static int[] FRAME_SIZE = {400, 400};
+
+    public static final int TEXTFIELD_SIZE = 15;
+}
